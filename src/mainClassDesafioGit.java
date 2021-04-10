05484bd100d09b34a6e465f07b450d4bc702de72
jcuellra <jcuellra@everis.com>
2021-04-10 21:02:29 +0200

    Segunda version EverisGit1InicialesNombre

--- src/mainClassDesafioGit.java
+++ src/mainClassDesafioGit.java
@@ -4,6 +4,8 @@ public class mainClassDesafioGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		
+		System.out.println("Hola mundo, saludos de Jesús Cuéllar!");
 	}
 
 }
